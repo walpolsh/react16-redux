3f638f144d1a0d9fe5163fa9d5fa1f2ee01adfc8
Paul Walsh <twentytwentyk@live.com>
2018-05-12 12:30:06 -0400

    build controls

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import { SSL_OP_NETSCAPE_CHALLENGE_BUG } from 'constants';
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => { // 'cheese', 'meat', ect...
+      return [...Array(props.ingredients[igKey])] // make array from the ingredients
+      .map((_, i) => { //_ is blank because we already have access to igKey, but we do need the element number
+        return <BurgerIngredient 
+          key={igKey + i} //meat1, cheese2, ect... arrays of JSX elements require key's
+          type={igKey} />
+      }); 
+    })
+    .reduce((prev, curr) => {
+      return prev.concat(curr)
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
+  
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
+        {transformedIngredients}
       <BurgerIngredient type='bread-bottom' /> 
     </div>
   );
